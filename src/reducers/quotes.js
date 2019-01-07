3244df8d3439ad374ae3ed8c60820fde64011550
Jason Campbell <jlcampbell86@gmail.com>
2019-01-07 15:21:26 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(q => action.quoteId !== q.id)
+    case 'UPVOTE_QUOTE':
+      return state.map(q => {
+        if(q.id === action.quoteId){
+          return {...q, votes: q.votes + 1}
+        } else {
+            return q
+        }
+      })
+    case 'DOWNVOTE_QUOTE':
+    return state.map(q => {
+      if(q.id === action.quoteId && q.votes > 0){
+        return {...q, votes: q.votes - 1}
+      } else {
+          return q
+      }
+    })
+    default:
+      return state;
+  }
 }
