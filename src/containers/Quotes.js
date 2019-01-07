628f2689274edc2b79911b8e6de649519bb95d1c
jennyjean <jennyjean8675309@yahoo.com>
2019-01-07 18:41:40 -0500

    Built an application out more fully using Redux

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes'
+
 
 class Quotes extends Component {
 
@@ -20,6 +22,15 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+               {this.props.quotes.map(q => (
+                 <QuoteCard
+                   key={q.id}
+                   quote={q}
+                   removeQuote={() => this.props.removeQuote(q.id)}
+                   upvoteQuote={() => this.props.upvoteQuote(q.id)}
+                   downvoteQuote={() => this.props.downvoteQuote(q.id)}
+                 />
+               ))}
             </div>
           </div>
         </div>
@@ -28,5 +39,15 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state =>{
+  return { quotes: state.quotes }
+}
+
+// const mapDispatchToProps = dispatch =>{
+//   return {
+//     removeQuote: () => dispatch(removeQuote())
+//   }
+// }
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
-export default (state = [], action) => {
-  return state;
+function quotesReducer(state = [], action){
+  let idx;
+  switch (action.type){
+    case 'ADD_QUOTE':
+      return [...state, {
+        content: action.quote.content,
+        author: action.quote.author,
+        id: action.quote.id,
+        votes: action.quote.votes
+      }];
+    case 'REMOVE_QUOTE':
+      const quote = state.find(q => q.id === action.quoteId)
+      idx = state.indexOf(quote);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    case 'UPVOTE_QUOTE':
+      const upvotedQuote = state.find(q => q.id === action.quoteId)
+      upvotedQuote.votes = upvotedQuote.votes + 1
+      return [...state]
+    case 'DOWNVOTE_QUOTE':
+      const downvotedQuote = state.find(q => q.id === action.quoteId)
+      if (downvotedQuote.votes === 0){
+        return state
+      } else {
+        downvotedQuote.votes = downvotedQuote.votes - 1
+        return [...state]}
+    default:
+      return state
+
+  }
 }
+
+export default quotesReducer;
